9541d4900a79e2c94322ffbd2209063f0f4f685e
Nishchal Gaba <nishgaba.ai@gmail.com>
2022-01-08 21:36:32 +0530

    Update AaveV2 Current Var for latest datetime

--- src/protocol/aave_v2/var.py
+++ src/protocol/aave_v2/var.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 from io import StringIO
 from datetime import timedelta
-from datetime import datetime, timezone
+from datetime import datetime
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
@@ -158,106 +158,37 @@ def change():
 def main(look_back=1, duration = 180):
 
     transactionResponse = {}
-
-    names = {"DAI"  : 'dai',
-            "GUSD"  : 'gemini-dollar',
-            # "SUSD"  : 'susd',
-            "TUSD"  : 'true-usd',
-            "USDC"  : 'usd-coin',
-            # "USDP"  : 'usdp',
-            "USDT"  : "tether",
-            "BAL"   : "balancer",
-            "WETH"   : "ethereum",
-            "LINK"  : "chainlink",
-            "MKR"   : "maker",
-            "RAI"   : "rai",
-            "UNI"   : "uniswap",
-            "WBTC"  : "wrapped-bitcoin",
-            "XSUSHI": "xsushi",
-            "YFI"   : "yearn-finance",
-            "BUSD"  : "binance-usd",
-            "FEI"   : "fei-usd",
-            "FRAX"  : "frax",
-            "AAVE"  : "aave",
-            "AMPL"  : "ampleforth",
-            "BAT"   : "basic-attention-token",
-            "CRV"   : "curve-dao-token",
-            "DPI"   : "defipulse-index",
-            # # "ENJ"   : "enjin-coin",
-            "KNC"   : "kyber-network-crystal",
-            "MANA"  : "decentraland",
-            # "REN"   : "ren",
-            "RENFIL": "renfil",
-            # "SNX"   :"synthetix-network-token",
-            "ZRX"   : "0x"
-            }
-
     errors = []
-    # transactionResponse = {"statusCode":200,"response":{}}
     transactionResponse = {"result":{"AAVEV2":{"data":{}}}}
-    ind       = 0
-    look_back = 1
-    query     = '''{reserves
-        (where:{
-            symbol:"%s"
-        })
-        {
-            name
-            symbol
-                reserveFactor
-            decimals
-            utilizationRate
-            availableLiquidity
-            totalLiquidity
-            reserveFactor
-        }}'''
-
-    df1 = pd.DataFrame(columns=['Asset', 'CurrentPrice', 'availableLiquidity','decimals', 'totalLiquidity','utilizationRate'])
-    for asset in names.keys():
+    ind              = 0
+    look_back        = 1
+    current_date     = str((datetime.now() - timedelta(1)).date().strftime("%m-%d-%Y"))
+    AAVE_API_RESULT  = requests.get('https://aave-api-v2.aave.com/data/liquidity/v2?poolId=0xb53c1a33016b2dc2ff3653530bff1848a515c8c5&date='+current_date).json() #12-14-2021'
+    AAVE_DATA        = pd.DataFrame(columns=['Token', 'CurrentPrice', 'availableLiquidity','decimals', 'totalLiquidity','utilizationRate','Assets'])
+    for token in AAVE_API_RESULT:
         try:
-            print(names[asset])
-            request = requests.get("https://api.coingecko.com/api/v3/coins/"+ str(names[asset]) + "/market_chart?vs_currency=usd&days="+ str(look_back) +"&interval=daily")
-            data1   = request.json()
-            price   =  data1['prices'][0][1]
-            
-            V2_sample_transport       = RequestsHTTPTransport(
-                url='https://api.thegraph.com/subgraphs/name/aave/protocol-v2',
-                verify=True,
-                retries=5)
-            V2_client                 = Client(transport=V2_sample_transport)
-            response                  = V2_client.execute(gql(query % asset ))['reserves'][0]
-            
-            df1.loc[ind] = [asset] + [price] + [float(response['availableLiquidity'])] + [response['decimals']]+ \
-                                    [float(response['totalLiquidity'])]+ [float(response['utilizationRate'])]
+            AAVE_DATA.loc[ind] = [token['symbol']] + [float(token['referenceItem']['priceInUsd'])] + \
+                            [float(token['availableLiquidity'])] + [float(token['decimals'])]+ \
+                            [float(token['totalLiquidity'])]+ [float(token['referenceItem']['utilizationRate'])] + \
+                            [float(token['totalDebt'])]
+                                
             ind += 1
 
         except Exception as e:
-            print("**", asset, e) #str(asset), str(price_mean), str(price_std), str(cap_mean),str(cap_std), str(vol_mean) , str(vol_std))  
-            errors.append(asset)
+            print("**", token, e) #str(asset), str(price_mean), str(price_std), str(cap_mean),str(cap_std), str(vol_mean) , str(vol_std))  
+            errors.append(token)
             # transactionResponse["error"] = True
         transactionResponse["result"]["AAVEV2"]["context"] = {}
         transactionResponse["result"]["AAVEV2"]["context"]["errors"] = errors
 
 
-    df1['availableLiquidity']       = pd.to_numeric(df1['availableLiquidity'])
-    df1['utilizationRate']          = pd.to_numeric(df1['utilizationRate'])
-    df1['decimals']                 = pd.to_numeric(df1['decimals'])
-    df1['CurrentPrice']             = pd.to_numeric(df1['CurrentPrice'])
-    df1['MARKET']                   = (df1['availableLiquidity'] / (1 - df1['utilizationRate']))
-    df1['MARKET']                   = df1['MARKET'] / pow(10, df1['decimals'])
-    df1['totalBorrow']              = df1['MARKET']  * df1['utilizationRate'] 
-    df1['totalBorrow']              = df1['totalBorrow'] / pow(10, df1['decimals'])
-    df1['availableLiquidity']       = df1['availableLiquidity'] / pow(10, df1['decimals'])
-    df1['position']                 = df1['totalBorrow'] - df1['MARKET']  # df1['availableLiquidity']
-    df1['Asset($)']                 = df1['CurrentPrice'] * df1['position']
-    df1['Asset($,Billion)']         = df1['Asset($)'] / pow(10, 9)
-
-    # TOTAL_ASSETS = df1['totalBorrow'].sum()
-    # TOTAL_LIABILITIES = df1['MARKET'].sum()
-
+    # totalLiquidity == Market
+    AAVE_DATA['totalLiquidity']       = AAVE_DATA['totalLiquidity'] * AAVE_DATA['CurrentPrice']
+    AAVE_DATA['Assets']               = AAVE_DATA['Assets'] * AAVE_DATA['CurrentPrice']
+    AAVE_DATA['Position']             = AAVE_DATA['Assets'] - AAVE_DATA['totalLiquidity']
+    AAVE_DATA['Position($,Billion)']  = AAVE_DATA['Position'] / pow(10, 9)
 
     df = change()
-
 
     REQUIRED_1 = []
     REQUIRED_10 = []
@@ -267,31 +198,27 @@ def main(look_back=1, duration = 180):
         required_row = required_row[1]
         SUM_10 = []
         SUM_1 = []
-        for row in df1.iterrows():
-            SUM_10.append((row[1]['Asset($,Billion)'] * float(required_row[ row[1]['Asset'] +'_%change(10)'])/100))
-            SUM_1.append((row[1]['Asset($,Billion)'] * float(required_row[ row[1]['Asset'] +'_%change(1)'])/100))
-        
+            
+        for row in AAVE_DATA.iterrows():
+            try:
+                SUM_10.append((row[1]['Position($,Billion)'] * float(required_row[ row[1]['Token'] +'_%change(10)'])/100))
+                SUM_1.append((row[1]['Position($,Billion)'] * float(required_row[ row[1]['Token'] +'_%change(1)'])/100))
+            except:
+                # print("ERROR")
+                pass
+
         REQUIRED_1.append(sum(SUM_1))
         REQUIRED_10.append(sum(SUM_10))
-        
+
         historicalVAR.loc[hist_ind] = [required_row['date']] + [sum(SUM_10)] + [sum(SUM_1)]
         hist_ind += 1
     
+
+
+
     print("DEBUG ", historicalVAR.shape)
+    historicalVAR = historicalVAR[:362]
 
-    # ALL_TIME_VAR_10 = historicalVAR['VAR_10'].min()
-    # ALL_TIME_VAR_1 = historicalVAR['VAR_1'].min()
-    # ALL_TIME_VAR_10_DATE = historicalVAR['date'][historicalVAR['VAR_10'].idxmin()]
-    # ALL_TIME_VAR_1_DATE = historicalVAR['date'][historicalVAR['VAR_1'].idxmin()]
-
-    # REQUIRED_10 = REQUIRED_10[:365]
-    # REQUIRED_1 = REQUIRED_1[:365]
-
-    historicalVAR = historicalVAR[:365]
-
-    
-    # REQUIRED_10.sort()#(reverse=True)
-    # REQUIRED_1.sort()#(reverse=True)
     REQUIRED_10 = historicalVAR.sort_values('VAR_10').iloc[3]
     REQUIRED_1 = historicalVAR.sort_values('VAR_1').iloc[3]
 
@@ -304,32 +231,15 @@ def main(look_back=1, duration = 180):
     var_date_1_95p = REQUIRED_1_95['date']
 
 
-    # dt = datetime.now()
-    # dt = dt.replace(tzinfo=timezone.utc)
-    # print(dt)
-    print("Updated to current date")
-    current_date = str(datetime.now().date().strftime("%m-%d-%Y"))
-    
-    print('https://aave-api-v2.aave.com/data/liquidity/v2?poolId=0xb53c1a33016b2dc2ff3653530bff1848a515c8c5&date='+current_date)
-    res = requests.get('https://aave-api-v2.aave.com/data/liquidity/v2?poolId=0xb53c1a33016b2dc2ff3653530bff1848a515c8c5&date='+current_date).json() #12-14-2021'
-    print("Print Test Res")
-    print(res)
-
     df = pd.DataFrame(columns=['SYMBOL', 'totalLiquidity', 'totalDebt'])
     ind = 0
-    for i in res:
+    for i in AAVE_API_RESULT:
         df.loc[ind] = [i['symbol']] + [float(i['totalLiquidity']) * float(i['referenceItem']['priceInUsd']) ]\
                     + [float(i['totalDebt']) * float(i['referenceItem']['priceInUsd'] )] #+ [i['referenceItem']['priceInUsd'] * ]
         ind += 1
 
-    TOTAL_LIABILITIES = df['totalLiquidity'].sum()
-    TOTAL_ASSETS      = df['totalDebt'].sum()
-
-
-    # response["statusCode"] = 200
-    # print(REQUIRED_10[:10])
-    # transactionResponse["result"]["AAVEV2"]["data"]['10_day_99p'] = str(REQUIRED_10[5])
-    # transactionResponse["result"]["AAVEV2"]["data"]['10_day_99p'] = str(REQUIRED_10[4])
+    TOTAL_LIABILITIES = AAVE_DATA['totalLiquidity'].sum()
+    TOTAL_ASSETS      = AAVE_DATA['Assets'].sum()
     transactionResponse["result"]["AAVEV2"]["data"]['10_day_99p'] = str(REQUIRED_10['VAR_10'])
     transactionResponse["result"]["AAVEV2"]["data"]['var_date_10_day_99p'] = var_date_10_99p
 
@@ -347,8 +257,6 @@ def main(look_back=1, duration = 180):
     transactionResponse["result"]["AAVEV2"]["data"]['relative_var_liabilities'] = str(REQUIRED_10['VAR_10'] * pow(10,9) / TOTAL_LIABILITIES)
     transactionResponse["result"]["AAVEV2"]["data"]['total_assets'] = TOTAL_ASSETS
     transactionResponse["result"]["AAVEV2"]["data"]['total_liabilities'] = TOTAL_LIABILITIES
-
-    
 
 
     return  transactionResponse
